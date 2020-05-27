bd877efcc225ae1080afec5372e3b9f9bc3b77b3
marleydebrah <65576029+marleydebrah@users.noreply.github.com>
2020-05-27 14:53:20 +1200

    Delete idle python - internet.py

--- idle python - internet.py
+++
@@ -1,9 +0,0 @@
-#Create variable
-hours_spent = int(input("How many hours do you spend on the internet"))
-
-#Print the result
-print("you spend {} hours on the internet." .format(hours_spent))
-
-#Add if/else statements based around results
-if hours_spent >=5:
-    print("you should go outside more!")
